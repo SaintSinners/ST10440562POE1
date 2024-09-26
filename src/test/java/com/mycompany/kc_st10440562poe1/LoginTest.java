0feac4236af297e4b7ae84d10c06bb7f640c2edb
RC_Student_lab <RC_Student_lab@10.0.0.211>
2024-09-26 17:18:32 +0000

    Username & Password Packs Completed

---
+++ src/test/java/com/mycompany/kc_st10440562poe1/LoginTest.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.kc_st10440562poe1;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoginTest {
+    @Test
+    public void testCheckUsername() {
+        System.out.println("CheckUsername");
+        String username = "engwe";
+        Login instance = new Login();
+        boolean expResult = false;
+        boolean result = instance.CheckUsername(username);
+        assertEquals(expResult, result);
+    }
+        @Test
+    public void testCheckUsernameTrue() {
+        System.out.println("CheckUsername");
+        String username = "engw_";
+        Login instance = new Login();
+        boolean expResult = true;
+        boolean result = instance.CheckUsername(username);
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testCheckPassword() {
+        System.out.println("checkPassword");
+        String password = "lionsden4d";
+        Login instance = new Login();
+        boolean expResult = false;
+        boolean result = instance.checkPassword(password);
+        assertEquals(expResult, result);
+    }
+        @Test
+    public void testCheckPasswordTrue() {
+        System.out.println("checkPassword");
+        String password = "Gideonwar@800";
+        Login instance = new Login();
+        boolean expResult = true;
+        boolean result = instance.checkPassword(password);
+        assertEquals(expResult, result);
+    }
+}
